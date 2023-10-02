2bc8e1efedf68f4cad4325bb826ce8b3619c4506
Francis Cawog <Franciscawog@gmail.com>
2023-10-02 17:11:20 -0400

    finished phase 4

--- practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/controllers/sessions_controller.rb
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/controllers/sessions_controller.rb
@@ -1,4 +1,7 @@
 class SessionsController < ApplicationController
+    before_action :require_logged_out, only: [:new, :create]
+    before_action :require_logged_in, only: [:destroy]
+
     def new
         @user = User.new
         render :new
@@ -11,7 +14,8 @@ class SessionsController < ApplicationController
             login(@user)
             redirect_to cats_url
         else
-            @user = {username: params[:user][:username]}
+            # @user = {username: params[:user][:username]}
+            @user = User.new(username: params[:user][:username])
             render :new
         end
     end
--- practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/controllers/users_controller.rb
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/controllers/users_controller.rb
@@ -1,7 +1,5 @@
 class UsersController < ApplicationController
     before_action :require_logged_out, only: [:new, :create]
-    before_action :require_logged_in, only: [:destroy]
-
 
     def new
         :new
